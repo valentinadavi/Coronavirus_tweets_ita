fb1bf619746b4fe02c5b3d2bce5035eb0f8d8a4b
Valentina Davì <valentina.davi1@studenti.unimi.it>
2020-02-24 20:51:32 +0100

    new try

--- Src/01_scraping.R
+++ Src/01_scraping.R
@@ -7,7 +7,19 @@ italian_coronav_tweets1_2402 <- search_tweets2(
   retryonratelimit = TRUE,
   include_rts = FALSE, 
   lang = "it",
-  since_id =  1229719602764099585
+  since_id =  1229719602764099585, 
+  
 )
+#retrieved 53681 tweets from 23 to 24 / 02 
 
 save(italian_coronav_tweets1_2402, file = "Italian_coronav_tweets_2402.RData")
+
+# new try with older tweets 
+search_tweets2(
+  c("coronavirus"), n = 50000, 
+  retryonratelimit = TRUE, 
+  include_rts = FALSE, 
+  lang = "it", 
+  since_id(1229719602764099585),
+  max_id(1231635361140543488)
+)

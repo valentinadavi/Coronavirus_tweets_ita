58537c6d99dece5e85715c0ace9a75e9b11a821b
Valentina Davì <valentina.davi1@studenti.unimi.it>
2020-02-24 19:08:26 +0100

    setting up

---
+++ Src/01_scraping.R
@@ -0,0 +1,10 @@
+# The purpose here is to continue a previous research 
+# The idea is to download italian tweets that contain the word coronavirus
+
+italian_coronav_tweets1_2402 <- search_tweets2(
+  c("coronavirus"), n = 50000, 
+  retryonratelimit = TRUE,
+  include_rts = FALSE, 
+  lang = "it",
+  since_id = 
+)

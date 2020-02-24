673e854ee256eb19870c6e63f8c6068c4e67d39f
Federica Giua <federica.giua@studenti.unimi.it>
2020-02-24 22:19:16 +0100

    scraping new tweets

---
+++ scr_01.R
@@ -0,0 +1,20 @@
+library(rtweet)
+coronavirus_lombardia <- search_tweets(
+  "#coronavirus OR coronavirus, #Lombardia OR Lombardia", 
+  n = 50000, 
+  retryonratelimit = TRUE, 
+  include_rts = FALSE, 
+  lang = "it"
+)
+
+save(coronavirus_lombardia, file = "coronavirus_lombardia.RData")
+
+coronavirus_veneto <- search_tweets(
+  "#coronavirus OR coronavirus, #Veneto OR Veneto", 
+  n = 50000, 
+  retryonratelimit = TRUE, 
+  include_rts = FALSE, 
+  lang = "it"
+)
+
+save(coronavirus_veneto, file = "coronavirus_veneto.RData")

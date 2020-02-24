61eb68631736bb7b1efc7344f4339ed61a7e38fc
Valentina Davì <valentina.davi1@studenti.unimi.it>
2020-02-24 20:09:28 +0100

    saved data

--- Src/01_scraping.R
+++ Src/01_scraping.R
@@ -1,10 +1,13 @@
 # The purpose here is to continue a previous research 
 # The idea is to download italian tweets that contain the word coronavirus
+library(rtweet)
 
 italian_coronav_tweets1_2402 <- search_tweets2(
   c("coronavirus"), n = 50000, 
   retryonratelimit = TRUE,
   include_rts = FALSE, 
   lang = "it",
-  since_id =  
+  since_id =  1229719602764099585
 )
+
+save(italian_coronav_tweets1_2402, file = "Italian_coronav_tweets_2402.RData")

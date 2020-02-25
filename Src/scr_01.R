dbd6879595acc258ee5963c1a9bb327046a71b9e
Federica Giua <federica.giua@studenti.unimi.it>
2020-02-25 10:32:17 +0100

    New obs collected

--- Src/scr_01.R
+++ Src/scr_01.R
@@ -2,7 +2,7 @@ library(rtweet)
 
 #Further reseach on coronavirus spreading in Italy
 
-#Retrieving
+#Coronavirus in Lombardia retrieving 11193 obs.
 coronavirus_lombardia <- search_tweets(
   "#coronavirus OR coronavirus, #Lombardia OR Lombardia", 
   n = 50000, 
@@ -13,6 +13,7 @@ coronavirus_lombardia <- search_tweets(
 
 save(coronavirus_lombardia, file = "coronavirus_lombardia.RData")
 
+#Coronavirus in Veneto
 coronavirus_veneto <- search_tweets(
   "#coronavirus OR coronavirus, #Veneto OR Veneto", 
   n = 50000, 
@@ -22,3 +23,14 @@ coronavirus_veneto <- search_tweets(
 )
 
 save(coronavirus_veneto, file = "coronavirus_veneto.RData")
+
+#Coronavirus in Piemonte
+coronavirus_piemonte <- search_tweets(
+  "#coronavirus OR coronavirus, #Piemonte OR Piemonte", 
+  n = 50000, 
+  retryonratelimit = TRUE, 
+  include_rts = FALSE, 
+  lang = "it"
+)
+
+save(coronavirus_piemonte, file = "coronavirus_piemonte.RData")

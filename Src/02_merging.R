d980411729922c43abb9a2765802a641cba543b0
Valentina Davì <valentina.davi1@studenti.unimi.it>
2020-03-05 23:33:35 +0100

    began merging the existent datasets

---
+++ Src/02_merging.R
@@ -0,0 +1,8 @@
+coronavirus_february <- rbind(italian_coronav_tweets1_2402, italian_coronav_tweets_2402b, italian_coronav_tweets_2702, 
+                              italian_coronav_tweets_2902)
+  
+coronavirus_march <- rbind(italian_coronav_tweets_0303, italian_coronav_tweets_0503)
+
+coronavirus_full_upto0503 <- rbind(coronavirus_february, coronavirus_march)
+
+save(coronavirus_full_upto0503, file = "Italian_coronav_tweets_allupto0503.RData")

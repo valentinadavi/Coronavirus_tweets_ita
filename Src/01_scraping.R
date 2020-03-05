3f02f47bbdea72832858612db1b93374145df7e8
Valentina Davì <valentina.davi1@studenti.unimi.it>
2020-03-05 23:19:40 +0100

    downloaded data on 5/03

--- Src/01_scraping.R
+++ Src/01_scraping.R
@@ -72,3 +72,15 @@ italian_coronav_tweets_0303 <- search_tweets2(
 #retreived tweets from 01 03 to 03 03. 
 
 save(italian_coronav_tweets_0303, file = "Italian_coronav_tweets_0303.RData")
+
+min_id_0303 <- since_id(italian_coronav_tweets_0303)
+#younger tweets of 2402b 
+italian_coronav_tweets_0503 <- search_tweets2(
+  c("coronavirus"), n = 50000, 
+  retryonratelimit = TRUE, 
+  include_rts = FALSE, 
+  lang = "it", 
+  since_id = min_id_0303) 
+#retreived tweets from 01 03 to 03 03. 
+
+save(italian_coronav_tweets_0503, file = "Italian_coronav_tweets_0503.RData")
